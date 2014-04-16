4e4489ecb544b7ab1f7abfc93b537946782f7f8e
lyj <latyas@gmail.com>
2014-04-17 00:32:19 +0800

    文件名忘了改了

--- for-OpenWRT/install.sh
+++ for-OpenWRT/install.sh
@@ -1,4 +1,4 @@
 cp ./drcom.conf /etc
 cp ./wired.py /usr/bin
-cp ./lucijludrcom.lua /usr/lib/lua/luci/controller/
+cp ./lucijludrcom.lua /usr/lib/lua/luci/controller/jludrcom.lua
 cp ./jludrcom.lua /usr/lib/lua/luci/model/cbi/
