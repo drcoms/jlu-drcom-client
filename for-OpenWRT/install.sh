1e1bad1442b313af92fb203231782914ffa3d430
uzanief <191057326@qq.com>
2015-10-08 00:42:31 +0800

    Update install.sh

--- for-OpenWRT/install.sh
+++ for-OpenWRT/install.sh
@@ -1,4 +1,4 @@
 cp ./drcom.conf /etc
 cp ./wired.py /usr/bin
-#cp ./lucijludrcom.lua /usr/lib/lua/luci/controller/jludrcom.lua
+#cp ./lucijludrcom.lua /usr/lib/lua/luci/controller/jludrcom.lua 注释掉了过期的lua
 #cp ./jludrcom.lua /usr/lib/lua/luci/model/cbi/
